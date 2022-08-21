d184b7c42af85bdda2428779420719d85bd15099
Paul <pavel_poyankov@mail.ru>
2022-08-21 18:19:07 +0300

    Объявили основные типы и операции

--- KNNnaive.h
+++ KNNnaive.h
@@ -1,5 +1,19 @@
 #pragma once
+
+#include <vector>
+#include "vecoperation.h"
+
 class KNNnaive
 {
+	private:
+		vecop::features* __X;
+		vecop::class_label* __Y;
+		int __k;
+
+	public:
+		KNNnaive(int k = 3);
+
+		void fit(const vecop::features& X, const vecop::class_label& Y);
+		void predict(vecop::feature X);
 };
 
---
+++ vecoperation.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+
+namespace vecop
+{
+	typedef std::vector<double> feature;
+	typedef std::vector<feature> features;
+	typedef std::vector<int> class_label;
+}
